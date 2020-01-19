c205cfa7356c8355ea593f5d38f89437dfd221ea
wysockilukas <42555510+wysockilukas@users.noreply.github.com>
2020-01-19 20:51:01 +0100

    podstawy komponentow

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,82 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
+//! Wazne Person to nasza nazwa komponentu, nie musi byćPerosn, ale powinnop być z dużej litery
+// W React komponenty z małej litery są zarezerwowany na elemnty HTML
+import Person from './Person/Person';  
+
+class App extends Component {
+
+  _self = this
+  // Tworzymy state - state t- zarezerwowany słowo
+  state = {
+    persons: [
+      {name:"Jan",age:"67"},
+      {name:"Tomasz",age:"44"},
+      {name:"Zosia"}
+    ]
+  }
+
+  switchNameHandler = (newName) => {
+    console.log("klilniety", this.state);
+    console.log("self ", this._self);
+    console.log("this ", this);
+
+    //this.state.persons[0] = "Pola"; to nie dzaiała
+    this.setState({
+      persons: [
+        {name:newName,age:"11"},
+        {name:"Tomasz",age:"44"},
+        {name:"Zosia"}
+      ]
+    } )
+  }
+
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        {name:"Jan",age:"11"},
+        {name: event.target.value,age:"44"},
+        {name:"Zosia"}
+      ]
+    } )
+  }
+
+
+  render() {  //jsx kompiluje się do kodu ponziej .. React.creatElement...
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px'
+    }
+    return (
+      <div className="App">
+        <p>Hi, I'm a React App</p>
+        <p>Zwykły paragraph</p>
+        {/* <button onClick={this.switchNameHandler.bind(this, "Pola")}>switch</button> */}
+        <button 
+          style = {style}
+          onClick={() => this.switchNameHandler("Pola") }>switch</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age ={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age ={this.state.persons[1].age} 
+          click={this.switchNameHandler.bind(this, "You !!!")}
+          changed = {this.nameChangeHandler}
+          ><p>Hobby: racing</p></Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age ={this.state.persons[2].age} />
+      </div>      
+    )
+    // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Jakis tekst')) 
+  }
+}
+
+/*
 function App() {
   return (
     <div className="App">
@@ -8,5 +84,5 @@ function App() {
     </div>
   );
 }
-
+*/
 export default App;
---
+++ react-complete-guide/src/Person/Person.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import  './Person.css';  
+
+
+
+//! Ten component jest stateless bo nie ma w nim żadnego zarządznia stanami
+// Ten kocomponent tylko otrzymuje dane 
+//atrybut click wystepuje w app.js w wywołaniu elementu Person
+const person = (props)  => {
+return (
+    <div className="Person">
+        <p onClick={props.click}>I'm as {props.name} and I'm {Math.floor(Math.random()*60)} or {props.age} years old</p> 
+        <p>{props.children}</p>
+        {/* Ponizj mamy two-way binding */}
+        <input type = "text" onChange={props.changed} value={props.name} />
+    </div>
+)
+};
+
+export default person;
