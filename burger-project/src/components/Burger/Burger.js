303aa8a878ddde84116f919437a4909ad4217029
wysockilukas <42555510+wysockilukas@users.noreply.github.com>
2020-01-28 21:33:13 +0100

    rysowanie burgera na podstawie state

--- burger-project/src/components/Burger/Burger.js
+++ burger-project/src/components/Burger/Burger.js
@@ -2,13 +2,34 @@ import React, { useCallback } from 'react';
 import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
+// ingredients to obiekt zawierajacy dodatek i ich ilość 
+// ale cchemy zeby byl tablicą, bo wtedy będziemy mogli użyc funkcji map
 const burger= (props) => {
+
+    let arrIngredients = Object.keys(props.ingredients).map( igKey => {  // igKey np salad
+        // return [...Array( props.ingredients[igKey]  )].map( (_,i) => {
+        //     <BurgerIngredient key={igKey + i} type={igKey} />
+        // } )
+        // console.log([...Array( props.ingredients[igKey]  )]);
+        let a = [...Array( props.ingredients[igKey]  )].map( (_,i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey} />;
+        })
+        // console.log(a);
+         return a;
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+    // console.log(arrIngredients);
+
+    if (arrIngredients.length ===0 ) {
+        arrIngredients = <p>Dodaj jakies skladniki</p>
+    }
 
     return (
         <div className = {classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />     
+                {arrIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
         
--- burger-project/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-project/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,17 +2,27 @@ import React, { Component } from 'react';
 import ReactAux from '../../hoc/ReactAux';
 
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 /*
 Ten komponent jest w folderze containers bo bedzie zareządzał stanami
 */
 class BurgerBuilder extends Component {
 
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
     render () {
         return (
             <ReactAux>
-                <Burger />
-                <div>Build Controls</div>
+                <Burger ingredients = {this.state.ingredients}/>
+                <BuildControls />
             </ReactAux>
         );
 
