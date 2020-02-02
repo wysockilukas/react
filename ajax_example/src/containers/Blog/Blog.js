5b04eb87a249961ca1c858fee51d97e97e44ef95
wysockilukas <42555510+wysockilukas@users.noreply.github.com>
2020-02-02 21:41:22 +0100

    axios get i componetUpdate

--- ajax_example/src/containers/Blog/Blog.js
+++ ajax_example/src/containers/Blog/Blog.js
@@ -1,21 +1,57 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import post from '../../components/Post/Post';
 
 class Blog extends Component {
+
+    state = {
+        posts: [],
+        selectedPostId: null
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then( res => {
+                const posts  = res.data.slice(0,4);
+                const updatedPosts = posts.map( el => {
+                    return {
+                        ...el,
+                        author: 'Max'
+                    }
+                })
+                this.setState({posts:  updatedPosts});
+            });
+    }
+
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId:id});
+    }
+
+
     render () {
+
+        const posts = this.state.posts.map( (el,idx) => {
+            return <Post 
+                key={el.id} 
+                title={el.title} 
+                author={el.author}
+                clicked = { () =>  this.postSelectedHandler(el.id)}
+                />
+        })
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
