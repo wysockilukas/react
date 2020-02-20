998f78b571cc54f8fd2e601e3dc05b7506e348e5
wysockilukas <42555510+wysockilukas@users.noreply.github.com>
2020-02-20 21:37:07 +0100

    1

---
+++ burger-project-redux-auth/src/containers/Auth/Auth.js
@@ -0,0 +1,136 @@
+import React, {Component} from 'react';
+import Input from '../../components/UI/Input/Input';
+import Button from '../../components/UI/Button/Button';
+
+import classes from './Auth.module.css';
+import * as actions from '../../store/actions/index';
+
+
+class Auth extends Component {
+
+    state = {
+        controls: {
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Email adres'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    isMail: true
+                },
+                valid: false,
+                touched: false
+            },
+            password: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'password',
+                    placeholder: 'Password'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 6
+                },
+                valid: false,
+                touched: false
+            }            
+        },
+        formValid: false
+    }
+
+
+    submitHandler = (event) => {
+        event.preventDefault();
+        const formData = {};
+        Object.keys(this.state.controls).forEach( name =>  {
+            formData[name] = this.state.controls[name].value;
+        }  );
+        
+        console.log(formData);
+    }
+
+
+    checkValidity = (value, rules) => {
+        if (Object.keys(rules).length === 0) return true;
+        let isValid = false
+        if (rules.required) {
+           isValid = (value.trim() !=='') 
+        }
+        if (rules.minLenght) {
+            isValid = (value.length >=rules.minLenght)
+        }
+        return isValid;
+    }
+
+    inputChangedHandler = (event, inputIdentifier) => {
+        // console.log(event.target.value);
+        const updatedOrderForm = {
+            ...this.state.controls  //ten kolon nie jest gleboki bo biekt jest nested wiec zrobimy ten trik na tym co nam zwroci
+        };
+        const updatedOrderFormElement = {...updatedOrderForm[inputIdentifier]};
+        updatedOrderFormElement.value = event.target.value;
+        updatedOrderFormElement.valid = this.checkValidity(updatedOrderFormElement.value, updatedOrderFormElement.validation);
+        updatedOrderFormElement.touched = true;
+
+
+        updatedOrderForm[inputIdentifier] = updatedOrderFormElement;
+
+
+        let formIsValid = true;
+        for (let k in updatedOrderForm) {
+            if (updatedOrderForm[k].validation) {
+                formIsValid = updatedOrderForm[k].valid && formIsValid ;
+            }
+        }
+
+        
+        this.setState({
+            controls:updatedOrderForm,
+            formValid: formIsValid
+        });
+    }
+   
+
+    render () {
+
+        const formElementsArray = [];
+        for (let key in  this.state.controls) {
+                formElementsArray.push( {
+                    id: key,
+                    config: this.state.controls[key]
+                } )
+        }
+        let form = (
+            <form onSubmit={this.submitHandler}>
+                {formElementsArray.map(el => (
+                    <Input 
+                        key={el.id} 
+                        name ={el.id} 
+                        elementType={el.config.elementType}  
+                        value={el.config.value}  
+                        elementConfig={el.config.elementConfig}
+                        invalid={!el.config.valid}
+                        shouldValidate={el.config.validation}
+                        touched={el.config.touched}
+                        changed = {(event) => this.inputChangedHandler(event,el.id)}
+                        />
+                    ))}
+            </form>
+        )
+ 
+
+        return (
+            <div className = {classes.Auth} >
+                {form}
+                <Button  disabled = {!this.state.formValid} btnType="Success"   value=""  >Subbmit</Button>
+            </div>
+        )
+    }
+}
+
+
+export default Auth;
