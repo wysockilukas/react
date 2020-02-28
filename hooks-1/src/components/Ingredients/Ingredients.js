1319d5fb02e56e88fb03388360990c2a3cf6948c
wysockilukas <42555510+wysockilukas@users.noreply.github.com>
2020-02-28 21:51:43 +0100

    useEffect

--- hooks-1/src/components/Ingredients/Ingredients.js
+++ hooks-1/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
+import axios from 'axios';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -7,8 +8,55 @@ import Search from './Search';
 function Ingredients() {
   const [ingredients, setIngredients] = useState([]);
 
+
+
+/*
+   //to sie odpala jak komponent sie wyrenderuje i dla kazdego cylu renderowania
+   // to jest podobne do componentDidUpdate, jesli jesy wywolane bez dodatkwoego arguemntu - dependecnies
+   // jesli jako drugi argiuemnt podamy [] pusta tablice to bedzie jak componetDidMount
+  useEffect(  () => {
+    axios.get('https://react-hooks-11471.firebaseio.com/ingredients.json')
+      .then( res => {
+          const fetchedData = Object.keys(res.data).map( key=> {
+            return {
+              id: key,
+              title: res.data[key].title,
+              amount: res.data[key].amount
+            }
+          })
+        // console.log('Otrzymalem ', fetchedData);
+        setIngredients(fetchedData);
+      }).catch (err => {
+        console.log('err', err);
+      })
+
+  }, [] );   
+
+*/
+
+  useEffect( () => {
+    console.log('RENDERING INGREDIENTS',);
+  }, [ingredients])  //uruchomi sie jak sie zmieniu ingredients
+
+
+  // useCallback chodzi o to zeby ta funckjja nie zmieniala sie z kazdym cyklem render
+  // bo jak sie zmienia to kompoment ktory ja otrzymuje  <search> mysli ze sie zmienia i eseEffect startuje
+  const filteredIngredientHandler = useCallback( (ing) => {
+    setIngredients(ing);
+  },  []);
+
   const AddIngredientsHandler = (ing) => {
-    setIngredients( prevIng => [...prevIng, {id: Math.random().toString(), ...ing}]     )
+
+    axios.post('https://react-hooks-11471.firebaseio.com/ingredients.json',ing)
+      .then( res => {
+          // console.log('res', res);
+          // setIngredients( prevIng => [...prevIng, {id: Math.random().toString(), ...ing}]     )
+          setIngredients( prevIng => [...prevIng, {id: res.data.name, ...ing}]     )
+      }).catch (err => {
+        console.log('err', err);
+      })
+
+
   }
 
   const RemoveItemHandler = (receivedId) => {
@@ -24,7 +72,7 @@ function Ingredients() {
       <IngredientForm onAddIngred={AddIngredientsHandler}/>
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredientHandler} />
         <IngredientList ingredients ={ingredients}   onRemoveItem={ RemoveItemHandler  }/>
       </section>
     </div>
