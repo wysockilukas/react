6e143513ec46c6abbfd930625c1d8bf0c3b45080
wysockilukas <42555510+wysockilukas@users.noreply.github.com>
2020-03-03 23:17:51 +0100

    reducers i context

--- hooks-1/src/components/Ingredients/Ingredients.js
+++ hooks-1/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useCallback} from 'react';
+import React, {useState, useEffect, useCallback, useReducer, useRef} from 'react';
 import axios from 'axios';
 
 import IngredientForm from './IngredientForm';
@@ -8,14 +8,53 @@ import ErrorModal from '../UI/ErrorModal';
 
 import {getData, getData2} from '../../utils/fetchData';
 
-function Ingredients() {
-  const [ingredients, setIngredients] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [pracownik, setPracownik] = useState('');
-  const [error, setError] = useState();
 
+// to jest poza glowna funckja , ale moze byc tez wewntarz jesli potrzebujemy propsy
+const ingredientReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case 'SET':
+      return action.ingredients;
+    case 'ADD':
+      return [...currentIngredients, action.ingredients];
+    case 'DELETE':
+      return currentIngredients.filter( (el) => el.id !== action.ingredientId);
+    default:
+        throw new Error('Nie powiene tu dojsc');
+  }
+}
+
+const httpReducer = (httpState, action) => {
+  switch (action.type) {
+    case 'SEND':
+      return {...httpState, isLoading:true};
+    case 'RESPONSE':
+      return {isLoading:false, error: null};
+    case 'ERROR':
+      return {isLoading:false, error: action.errorData};   
+    default:
+        throw new Error('Nie powiene tu dojsc');
+  }
+}
+
+
+
+
+function Ingredients() {
+
+
+  const [ingredients, dispatch] = useReducer(ingredientReducer, []);  //drugi parametr jest opcjonalny, to inicjlany stan, u nas pusta tablica
+  const [httpState, dispatchHttp] = useReducer(httpReducer, {isLoading:false, error:null}); 
+
+  // const [ingredients, setIngredients] = useState([]);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState();
+
+
+  const [pracownik, setPracownik] = useState('');
 
   
+ // reducer to funcke ktora biora jakis input i zwracaja output
+
 
 /*
    //to sie odpala jak komponent sie wyrenderuje i dla kazdego cylu renderowania
@@ -56,39 +95,48 @@ function Ingredients() {
   // useCallback chodzi o to zeby ta funckjja nie zmieniala sie z kazdym cyklem render
   // bo jak sie zmienia to kompoment ktory ja otrzymuje  <search> mysli ze sie zmienia i eseEffect startuje
   const filteredIngredientHandler = useCallback( (ing) => {
-    setIngredients(ing);
+    // setIngredients(ing);
+    dispatch({type: 'SET', ingredients:ing});
   },  []);
 
   const AddIngredientsHandler = (ing) => {
-    setIsLoading(true);
+    // setIsLoading(true);
+    dispatchHttp({type:'SEND'});
     axios.post('https://react-hooks-11471.firebaseio.com/ingredients.json',ing)
       .then( res => {
           // console.log('res', res);
           // setIngredients( prevIng => [...prevIng, {id: Math.random().toString(), ...ing}]     )
-          setIngredients( prevIng => [...prevIng, {id: res.data.name, ...ing}]     )
-          setIsLoading(false);
+          // setIngredients( prevIng => [...prevIng, {id: res.data.name, ...ing}]     )
+          dispatch({type:'ADD', ingredients: {id: res.data.name, ...ing}});
+          // setIsLoading(false);
+          dispatchHttp({type:'RESPONSE'});
+
       }).catch (err => {
         // console.log('err', err);
-        setError(err.message);
-        setIsLoading(false);
+        // setError(err.message);
+        // setIsLoading(false);
+        dispatchHttp({type:'ERROR', errorData:err.message});
       })
 
 
   }
 
   const RemoveItemHandler = (receivedId) => {
-    setIsLoading(true);
-
+    // setIsLoading(true);
+    dispatchHttp({type:'SEND'});
     axios.delete(`https://react-hooks-11471.firebaseio.com/ingredients/${receivedId}.json`)
       .then( res => {
-        setIngredients( prevIng => prevIng.filter( el => el.id !== receivedId));
-        setIsLoading(false);
+        // setIngredients( prevIng => prevIng.filter( el => el.id !== receivedId));
+        dispatch({type:'DELETE', ingredientId: receivedId});
+        // setIsLoading(false);
+        dispatchHttp({type:'RESPONSE'});
 
       }).catch (err => {
         //  console.log('err', err.message);
         //! jak w jednym bloku mamy dwa set... to sa uruchamiane razem i razem powoduje renedorwaie kompoinentu
-        setError(err.message);
-        setIsLoading(false);
+        // setError(err.message);
+        // setIsLoading(false);
+        dispatchHttp({type:'ERROR', errorData:err.message});
       })
 
   
@@ -99,14 +147,15 @@ function Ingredients() {
   }
 
   const clearError = () => {
-    setError(null); //jak zmieni sie state to renedruja sie strona i modal  z errorem nie wyskoczy
-    setIsLoading(false);
+    // setError(null); //jak zmieni sie state to renedruja sie strona i modal  z errorem nie wyskoczy
+    // setIsLoading(false);
+    dispatchHttp({type:'RESPONSE'});
   }
 
   return (
     <div className="App">
-      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
-      <IngredientForm onAddIngred={AddIngredientsHandler} loading={isLoading}/>
+      {httpState.error && <ErrorModal onClose={clearError}>{httpState.error}</ErrorModal>}
+      <IngredientForm onAddIngred={AddIngredientsHandler} loading={httpState.isLoading}/>
 
       <section>
         <Search onLoadIngredients={filteredIngredientHandler} />
